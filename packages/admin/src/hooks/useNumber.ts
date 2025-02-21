534ff7324a21d7359faf5f847a11435589e681d6
xyx650 <xyx650@gmail.com>
2025-02-21 23:40:56 +0800

    feat: 优化 useNumber 和 useTab 参数

--- packages/admin/src/hooks/useNumber.ts
+++ packages/admin/src/hooks/useNumber.ts
@@ -1,12 +1,20 @@
 import { useState } from 'react'
 
-export const useNumber = (defaultNum: number, token: string = 'USE_NUM'): [number, (num: number) => void] => {
+export interface IUseNumberOptions {
+  token?: string
+  storage?: 'session' | 'local'
+}
+
+export const useNumber = (defaultNum: number, {
+  token = 'USE_NUM',
+  storage = 'session'
+}: IUseNumberOptions = {}): [number, (num: number) => void] => {
   const key = `X-BLOG-NUM:${ token }`
-  // 获取 localStorage 的
-  const number = localStorage.getItem(key) || defaultNum
+  const Storage = storage === 'local' ? localStorage : sessionStorage
+  const number = Storage.getItem(key) || defaultNum
   const [num, _setNum] = useState(number)
   const setNum= (newNum: number) => {
-    localStorage.setItem(key, String(newNum))
+    Storage.setItem(key, String(newNum))
     _setNum(newNum)
   }
   return [+num, setNum]
--- packages/admin/src/hooks/useTab.ts
+++ packages/admin/src/hooks/useTab.ts
@@ -1,7 +1,14 @@
 import { useState } from 'react'
 import { useSearchParams } from '@umijs/max'
 
-export const useTab = <T extends string>(initialValue: T, tabKey: string = 'tab'): [T, (value: T) => void] => {
+export interface IUseTabOptions {
+  tabKey?: string
+  [p: string]: any
+}
+
+export const useTab = <T extends string>(initialValue: T, {
+  tabKey = 'tab',
+}: IUseTabOptions = {}): [T, (value: T) => void] => {
   const [query, setQuery] = useSearchParams()
   const value = query.get(tabKey) as T || initialValue
   const [curTabKey, setCurTabKey] = useState(value)
